ba5b1095e218e9f7dbc21331a0a1f8203814a32b
VishnuGopireddy <#Vishnu123>
2019-09-21 20:05:19 +0530

    push

---
+++ reverse_integer.py
@@ -0,0 +1,23 @@
+#https://leetcode.com/problems/reverse-integer/
+'''
+Example 2:
+Input: -123
+Output: -321
+'''
+class Solution:
+    def reverse(self, x):
+        num = x
+        if num >= 0:
+            num = str(num)[::-1]
+            num =  int(num)
+        else:
+            num = str(num)[1:]
+            num = -int(num[::-1])
+            num =  num
+        if -2**31 <= num <= (2**31)-1:
+            return num
+        else:
+            return 0
+
+s = Solution()
+print(s.reverse(1534236469))
