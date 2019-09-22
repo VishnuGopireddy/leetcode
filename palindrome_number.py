433866d92541c013ce6124663ddb84bc006232ad
vishnugopireddy <vishnu.var.reddy@gmail.com>
2019-09-22 23:04:55 +0530

    commit

---
+++ palindrome_number.py
@@ -0,0 +1,20 @@
+#https://leetcode.com/problems/palindrome-number/
+'''
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+'''
+
+
+class Solution:
+    def isPalindrome(self, x):
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return False
+
+s = Solution()
+print(s.isPalindrome(-121))
