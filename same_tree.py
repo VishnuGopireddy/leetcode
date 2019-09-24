4fe7b7ca83d6e0cdcb1bfe1af82d95ca4d044c71
VishnuGopireddy <vishnu.var.reddy@gmail.com>
2019-09-24 09:33:27 +0530

    push

--- same_tree.py
+++ same_tree.py
@@ -1,21 +1,3 @@
-#https://leetcode.com/problems/same-tree/
-'''
-Given two binary trees, write a function to check if they are the same or not.
-
-Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
-
-Example 1:
-
-Input:     1         1
-          / \       / \
-         2   3     2   3
-
-        [1,2,3],   [1,2,3]
-
-Output: true
-'''
-
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -24,24 +6,89 @@ Output: true
 #         self.right = None
 
 class Inorder:
-    def __init__(self,TreeNode):
+    def __init__(self):
         self.stack = []
-    def inorder(self,TreeNode):
-        root = TreeNode
+
+    def inorder(self, root):
         if root == None:
             return self.stack
         else:
             self.inorder(root.left)
-            self.stack.append(root.data)
+            self.stack.append(root.val)
+            print(root.val)
             self.inorder(root.right)
+
 
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        tree1 = Inorder(p)
-        tree2 = Inorder(q)
+        tree1 = Inorder()
+        tree2 = Inorder()
+        tree1.inorder(p)
+        tree2.inorder(q)
+
+        #        print(tree1.stack)
+        #        print(tree2.stack)
         if tree1 == tree2:
             return True
         else:
             return False
 
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
 
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            p = stringToTreeNode(line);
+            line = next(lines)
+            q = stringToTreeNode(line);
+
+            ret = Solution().isSameTree(p, q)
+
+            out = (ret);
+            print(out)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
