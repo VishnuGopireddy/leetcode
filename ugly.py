456e5c3216d0a9609a5db91de55a86afb65703e9
VishnuGopireddy <vishnu.var.reddy@gmail.com>
2019-09-25 10:51:25 +0530

    push

--- ugly.py
+++ ugly.py
@@ -1,5 +1,17 @@
+#https://leetcode.com/problems/ugly-number/
+'''
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+
+Example 1:
+Input: 6
+Output: true
+Explanation: 6 = 2 × 3
+
+'''
 def ugly(num):
     ugly = 0
+    if num < 1:
+        return False
     while num > 1 and ugly == 0:
         if num % 2 == 0:
             num = num // 2
@@ -9,7 +21,11 @@ def ugly(num):
             num = num // 5
         else:
             ugly = 1
-    return ugly
+    if ugly == 0:
+        return True
+    else:
+        return False
 
-num = 6
+
+num = 14
 print(ugly(num))
