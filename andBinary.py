c2d353d6e020f2eb10a5f8df7a4cff766e9245a5
VishnuGopireddy <vishnu.var.reddy@gmail.com>
2019-09-25 13:44:04 +0530

    push

---
+++ andBinary.py
@@ -0,0 +1,41 @@
+#Given two binary strings, return their sum (also a binary string).
+
+'''
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+'''
+
+
+def addBinary(a,b):
+    max_len = max(len(a),len(b))
+    a = a.zfill(max_len)
+    b = b.zfill(max_len)
+    result = []
+    i = 1
+    carry = 0
+    s=0
+    while(i <= max_len):
+        s = int(a[-i]) + int(b[-i]) + carry
+
+        if s == 1:
+            carry = 0
+        elif s == 2:
+            carry = 1
+            s = 0
+        elif s==3:
+            s = 1
+            carry = 1
+        result.append(str(s))
+        i = i + 1
+    if carry == 1:
+        result.append(str(carry))
+
+    result = result[::-1]
+    print(''.join(result))
+
+addBinary('101','101')
+
