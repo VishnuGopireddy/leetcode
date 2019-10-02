37dbc7a4e45d5bb7c05dca1f00beaa6c55a4cf53
vishnu <vishnu.var.reddy@gmail.com>
2019-10-02 21:39:07 +0530

    commit

--- remove element.py
+++ remove element.py
@@ -15,6 +15,7 @@ Your function should return length = 2, with the first two elements of nums bein
 It doesn't matter what you leave beyond the returned length.
 '''
 
+
 def removeElement(nums, val):
     while True:
         try:
