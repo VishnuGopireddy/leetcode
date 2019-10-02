6a6ebae939d6d9507035df0f7d3852a0e146f097
vishnu <vishnu.var.reddy@gmail.com>
2019-10-02 21:37:50 +0530

    commit

--- algorithms/binarysearch.py
+++ algorithms/binarysearch.py
@@ -3,8 +3,7 @@ nums = [1,4,6,7,9,11,15,18,19,23,28,32,33,38,41,46,48]
 element = 9
 
 
-def binary_search_iterative(nums,ele):
-    low = 0
+def binary_search_iterative(nums,ele):    low = 0
     high = len(nums)
     while low <= high:
         mid = (low + high) // 2
